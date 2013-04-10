7ab5d9d96f481ed826113f5b71e5c41fed3e0656
Stardrad Yin <yin8086@gmail.com>
2013-04-10 16:12:07 +0800

    change settings codec

--- main.cpp
+++ main.cpp
@@ -260,6 +260,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QSettings settings("UnitySuite.ini", QSettings::IniFormat);
+    settings.setIniCodec("UTF-8");
     int sptype;
 
     printf("Work in 3 modes:\n");
